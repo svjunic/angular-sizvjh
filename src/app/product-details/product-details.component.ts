9f8b89b1c2d4222d9878aa552423e7243c9bb46f
svjunic <sv.junic@gmail.com>
2020-04-06 14:27:38 +0900

    2 files modified, 3 added

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+
+import { products } from "../products";
+
+@Component({
+  selector: "app-product-details",
+  templateUrl: "./product-details.component.html",
+  styleUrls: ["./product-details.component.css"]
+})
+export class ProductDetailsComponent implements OnInit {
+  productId;
+  product;
+
+  constructor(private route: ActivatedRoute) {
+    console.log("ここは何で使うん？");
+  }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      console.log( params.get("productId") );
+      console.log( typeof params.get("productId") );
+      console.log( parseInt( params.get("productId"), 10 ));
+      this.productId = parseInt( params.get("productId"), 10 ) + 1;
+      this.product = products[this.productId];
+    });
+  }
+}
